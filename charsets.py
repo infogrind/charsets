d958589c17710336ec3b3cecda84609a5dfcaa49
Marius Kleiner <kleiner@gmail.com>
2022-11-19 11:51:30 +0100

    Handle non-printable characters
    For the special 'DEL' ASCII character, print 'DEL', and print a red 'x'
    for other non-printable characters (using the python `isprintable`
    function).

--- charsets.py
+++ charsets.py
@@ -3,7 +3,7 @@
 # Program to print single-byte characters using different character encodings.
 # This program also illustrates how to use Python's decode() method.
 
-CHAR_FROM = 32
+CHAR_FROM = 0
 CHAR_TO = 255
 
 charsets = {
@@ -14,14 +14,19 @@ charsets = {
         "utf-8": "utf-8",
         }
 
-print("\t".join(['#'] + list(charsets.keys())))
+def filter_printable(chars):
+    return map(
+            lambda c: c if c.isprintable() else '❌',
+            chars)
+
+print("\t".join(['Byte'] + list(charsets.keys())))
 for x in range(CHAR_FROM, CHAR_TO + 1):
     # First convert the integer to a corresponding (single) byte sequence.
     b = x.to_bytes(1, 'little')
     # For each specified charset, we convert the byte to a (single-character)
     # string by decoding using that specific charset.
     decoded = map(
-            lambda i: b.decode(encoding=charsets[i],errors='replace'),charsets.keys()
+            lambda i: 'DEL' if x == 127 else b.decode(encoding=charsets[i],errors='replace'),charsets.keys()
             )
     # Print nicely the character from each charset
-    print("\t".join(["{}".format(x)] + list(decoded)))
+    print("\t".join(["{}".format(hex(x))] + list(filter_printable(decoded))))
